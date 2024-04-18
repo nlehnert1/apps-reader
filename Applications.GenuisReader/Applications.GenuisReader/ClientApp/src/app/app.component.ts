d077f3bce054492d24620fca1e6f2f951aa4e2eb
Chloe Lehnert <44409145+nlehnert1@users.noreply.github.com>
2024-04-17 19:24:34 -0500

    Get Angular running (#7)
    * Added Home component and service, added CORS to C#, probably other stuff too
    
    * updated README for Angular instructions
    
    * cleanup

--- Applications.GenuisReader/Applications.GenuisReader/ClientApp/src/app/app.component.ts
+++ Applications.GenuisReader/Applications.GenuisReader/ClientApp/src/app/app.component.ts
@@ -2,11 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [],
-  template: `<h1>Default?asdf!</h1>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'Genius Reader';
 }
--- Applications.GenuisReader/Applications.GenuisReader/Program.cs
+++ Applications.GenuisReader/Applications.GenuisReader/Program.cs
@@ -1,3 +1,4 @@
+using GeniusReader.Domain.EFModel;
 using GeniusReader.WebApp;
 using GeniusReader.WebApp.Extensions;
 using Microsoft.AspNetCore.Hosting;
@@ -9,12 +10,20 @@ namespace Applications.GenuisReader
     {
         public static void Main(string[] args)
         {
+            var allowedOrigins = "_allowedOrigins";
             var builder = WebApplication.CreateBuilder(args);
-
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: allowedOrigins,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200", "https://localhost:4200");
+                    });
+            });
             // Add services to the container.
             var startup = new Startup(builder.Configuration);
             startup.ConfigureServices(builder.Services);
-
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -23,6 +32,7 @@ namespace Applications.GenuisReader
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+            app.UseCors(allowedOrigins);
 
             startup.Configure(app, builder.Environment);
         }
